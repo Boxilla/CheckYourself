b0c9f37d3fab37e740df60b8da18d0b6f731eee1
Botso <alejandro.quintana@sansano.sum.cl>
2016-11-16 18:03:51 -0300

    first commit

---
+++ Check_Yourself(LOGIN)/app/src/main/java/com/example/francisco_javier/check_yourselflogin/Registro.java
@@ -0,0 +1,15 @@
+package com.example.francisco_javier.check_yourselflogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
+
+
